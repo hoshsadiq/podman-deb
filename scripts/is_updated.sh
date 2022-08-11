f1610d74005339c8faf21a5aeba76b48bb14162c
Hosh Sadiq <hoshsadiq@users.noreply.github.com>
2022-08-11 10:58:37 +0100

    Fix triggering crun builds

--- scripts/is_updated.sh
+++ scripts/is_updated.sh
@@ -11,7 +11,7 @@ source "./$app/.env"
 
 tag_prefix="${app}-"
 upstream_tag="$(git ls-remote --tags "https://github.com/${repo:?}" | awk -F/ '$3 ~ /^v?[0-9]+(\.[0-9]+){1,2}$/{gsub(/^v/, "", $3); print $3}' | sort --version-sort --reverse | head -n1)"
-latest_tag="$(git ls-remote --tags "https://github.com/$current_repo" | awk -F/ '$3 ~ /^'"$tag_prefix"'[0-9]+(\.[0-9]+){2}$/{gsub(/^'"$tag_prefix"'/, "", $3); print $3}' | sort --version-sort --reverse | head -n1)"
+latest_tag="$(git ls-remote --tags "https://github.com/$current_repo" | awk -F/ '$3 ~ /^'"$tag_prefix"'[0-9]+(\.[0-9]+){1,2}$/{gsub(/^'"$tag_prefix"'/, "", $3); print $3}' | sort --version-sort --reverse | head -n1)"
 
 if [ "$get_latest" = "true" ] || [ "$upstream_tag" != "$latest_tag" ]; then
   printf "%s" "$upstream_tag"
